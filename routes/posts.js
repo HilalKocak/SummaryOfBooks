58d65c2d26c0aeafd509a2a8892b701a38160fe8
Hilal38 <hhilalkocak@gmail.com>
2024-02-16 22:37:20 +0300

    Test get post patch router methods of post

--- routes/posts.js
+++ routes/posts.js
@@ -2,61 +2,37 @@ const flatted = require('flatted')
 const User = require('../models/user')
 const {userService, postService, genreService, bookService} = require('../services') 
 
-const Book = require('../models/book')
-const Genre = require('../models/genre')
+
 
 const router = require('express').Router()
 
 router.get('/', async(req, res)=> {
-    const users = await bookService.load()
-    // res.send(flatted.stringify(users))
-    res.render('users', {users}) // res.render('users', {users:users})
+    const posts = await postService.load()
+    res.send({posts})
+    // res.render('posts', {posts}) 
 })
 
-router.get('/:userId', async(req, res)=> {
-    const user = await userService.find(req.params.userId)
-    if (!user) return res.status(404).send('Can not find user')
-    res.render('books_posts', {user}) // books_posts.pug
+//get genre with ID
+router.get('/:postId', async(req, res)=> {
+    const post = await postService.find(req.params.postId)
+    if (!post) return res.status(404).send('Can not find post')
+    res.send(post)
+    
 })
 
-
-//add post to user
-router.post('/:userId/post', async(req, res)=> {
-    const user = await userService.find(req.params.userId)
-    await user.addQuote(req.body.quote, user);
-    res.send(user)
-})
-
-
-//delete post from user
+//delete post
 router.delete('/delete-post/:postId', async(req, res) => {
-    await postService.removeBy('_id', req.params.userId)
+    await postService.removeBy('_id', req.params.postId)
     res.send('OK')
 })
 
-
-router.post('/:userId/post', async (req, res) => {
-    try {
-        const userId = req.params.userId;
-        const post = req.body.post;
-
-        // Kullanıcıya gönderi eklemek için addPostToUser fonksiyonunu kullan
-        const updatedUser = await addPostToUser(userId, post);
-
-        res.status(201).json({ message: 'Gönderi başarıyla kullanıcıya eklendi.', user: updatedUser });
-    } catch (error) {
-        console.error('Gönderi ekleme hatası:', error);
-        res.status(500).json({ error: 'Bir hata oluştu, gönderi eklenemedi.' });
-    }
-});
-
-// change one property of one record
-router.patch('/:bookId', async (req, res) => {
-  const { bookId }= req.params.bookId
-  const {name} = req.body
-
-  await bookService.update( bookId, { name })
-})
+router.patch('/:postId', async (req, res) => {
+    const postId = req.params.postId;
+    const { quote } = req.body;
+  
+    await postService.update(postId, { quote });
+    res.send('Updated!');
+  });
 
 
 module.exports = router
